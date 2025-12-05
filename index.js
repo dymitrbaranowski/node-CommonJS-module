027ad57fb2f763acd31c90e8e90d257516d06d5a
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-12-05 18:21:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// const nodemon = require("nodemon")
+
+const obj = require("./users");
+
+// console.log(obj);
+
+const { admins } = require("./users");
+console.log(admins);
---
+++ users.js
@@ -0,0 +1,10 @@
+const admins = ["Alex", "Andrej", "Vasiliy"];
+
+const clients = ["Anna", "Alina", "Tamara"];
+
+const users = {
+  admins,
+  clients,
+};
+
+module.exports = users;
